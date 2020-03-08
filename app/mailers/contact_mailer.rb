c194b7ab96ce5ceafe59db7bc468bc9b24bfbe3c
Marcin Tichoniuk <marcin@sology.eu>
2020-03-08 09:42:24 +0100

    Update comments in contact_mailer.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,8 @@ class ContactMailer < ActionMailer::Base
 
   default to: 'marcinxdar@wp.pl'
 
-  # name, email, body from app/controllers/contacts_controller.rb -> def create (name, email, comments)
+  # name, email, body from app/controllers/contacts_controller.rb -> def create (name, email, comments) -> save these 3 pieces of information to database 
+  # get name, email, body (aka comments) from database
   def contact_email(name, email, body)
     @name = name
     @email = email
