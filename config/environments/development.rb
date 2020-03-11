700824143482881d1ab74c3f70a30e4d27ace93a
Marcin Tichoniuk <marcin@sology.eu>
2020-03-11 18:29:52 +0100

    Add mailercatcher config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,8 @@ Rails.application.configure do
   end
 
  # Mailer configuration
- config.action_mailer.raise_delivery_errors = true
- config.action_mailer.default_url_options = { :host => "localhost:3000" }
+ config.action_mailer.raise_delivery_errors = false
+ config.action_mailer.default_url_options = { :host => "localhost:3001" }
  config.action_mailer.delivery_method = :smtp
  config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
  config.action_mailer.perform_caching = false
